766a50195eb88cd271d87bec239806d7c50eda57
ShashankMohan <Shashank.MohanUnnithan_1@philips.com>
2020-10-02 20:53:33 +0530

    checking to master

--- app/src/main/java/com/example/designpattern/MainActivity.java
+++ app/src/main/java/com/example/designpattern/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.designpattern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("TAG", "TAG");
     }
 }
